660aee715f34a53647d7388056e81edb5ae5c065
r31gN_ <the.reignn@gmail.com>
2016-08-25 15:21:16 +0300

    add placeholders, change model

--- Dropdown.elm
+++ Dropdown.elm
@@ -1,36 +1,41 @@
+module Dropdown exposing (init, view, update)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.App as App
 import Html.Events exposing (..)
 import Dropdown.DefaultStyles as DefaultStyles
+import String
 
 
 main : Program Never
 main =
     App.beginnerProgram {
-        model = model,
+        model = init "" ["2", "3", "4"] "Your age",
         view = view,
         update = update
     }
 
 
--- MODEL
-type alias Model =
+-- DROPDOWN MODEL
+type alias Dropdown =
     {
         value : String,
-        isOpen : Bool
+        valuesList : List String,        
+        placeholder : String,
+        isOpen : Bool        
     }
 
-model : Model
-model =
-    Model "" False
+init : String -> List String -> String -> Dropdown
+init initialValue valuesList placeholderValue =
+    Dropdown initialValue valuesList placeholderValue False
 
 
 -- UPDATE
 type Msg = 
     DropdownValue String | ToggleDropdown | HideDropdown
 
-update : Msg -> Model -> Model
+update : Msg -> Dropdown -> Dropdown
 update msg model =
     case msg of
         DropdownValue value ->
@@ -42,14 +47,15 @@ update msg model =
 
 
 -- VIEW
-view : Model -> Html Msg
+view : Dropdown -> Html Msg
 view model =
-    renderDropdownHtml model [1, 2, 3]
+    renderDropdownHtml model
 
 
 -- OTHER
-renderDropdownHtml : Model -> List valuesList -> Html Msg
-renderDropdownHtml model valuesList =
+
+renderDropdownHtml : Dropdown -> Html Msg
+renderDropdownHtml model =
     div
     [
         tabindex -1,
@@ -58,20 +64,20 @@ renderDropdownHtml model valuesList =
         onBlur HideDropdown
     ]    
     [
-        renderCaretValueHtml,
+        renderCaretHtml,
         renderDropdownValueHtml model,
-        renderDropdownListHtml model valuesList
+        renderDropdownListHtml model
     ]
 
-renderCaretValueHtml : Html Msg
-renderCaretValueHtml =
+renderCaretHtml : Html Msg
+renderCaretHtml =
     span
     [
         style DefaultStyles.dropdownCaretStyles
     ]
     []
 
-renderDropdownValueHtml : Model -> Html Msg
+renderDropdownValueHtml : Dropdown -> Html Msg
 renderDropdownValueHtml model =
     div
     [
@@ -80,11 +86,16 @@ renderDropdownValueHtml model =
         onClick ToggleDropdown
     ]
     [
-        text model.value
+        text (
+            if String.length model.value == 0
+                then model.placeholder 
+            else 
+                model.value
+        ) 
     ]
 
-renderDropdownListHtml : Model -> List valuesList -> Html Msg
-renderDropdownListHtml model valuesList =
+renderDropdownListHtml : Dropdown -> Html Msg
+renderDropdownListHtml model =
     ul
     [
         class "elm-dropdown__list",
@@ -96,10 +107,10 @@ renderDropdownListHtml model valuesList =
             [
                 class "elm-dropdown__list__item",
                 style DefaultStyles.dropdownListItemStyles,
-                onClick (DropdownValue (toString val))
+                onClick (DropdownValue val)
             ]
             [
-                text (toString val)
+                text val
             ]
-        ) valuesList
+        ) model.valuesList
     )
